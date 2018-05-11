30abe3307ff74e5b7a68f76d0ced3155326647d0
Jeroen Hijzelendoorn <j.m.hijzelendoorn@students.uu.nl>
2018-05-11 15:38:22 +0200

    oui

--- Code/Camera.cs
+++ Code/Camera.cs
@@ -17,10 +17,6 @@ class Camera
 
     void CameraPlacement()
     {
-        var M = Matrix4.CreatePerspectiveFieldOfView(1.6f, 1.3f, .1f, 1000);
-        GL.LoadMatrix(ref M);
-        GL.Translate(0, 0, -1);
-        GL.Rotate(110, 1, 0, 0);
-        GL.Rotate(a * 180 / PI, 0, 0, 1);
+
     }
 }
