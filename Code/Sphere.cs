e5ff936f89cb6e2cb064f5518dad5f1357967ded
Robert van den Brink <robertvdenbrink@gmail.com>
2018-05-15 11:05:23 +0200

    Sphere is better

--- Code/Sphere.cs
+++ Code/Sphere.cs
@@ -3,10 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using template.Code;
 
-namespace template.Code
+class Sphere : Primitive
 {
-    class Sphere : Primitive
-    {
-    }
+
 }
