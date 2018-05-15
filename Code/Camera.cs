578e178aa12046d871120aec66ac2493048692f3
Jeroen Hijzelendoorn <j.m.hijzelendoorn@students.uu.nl>
2018-05-15 12:19:33 +0200

    Small tings

--- Code/Camera.cs
+++ Code/Camera.cs
@@ -3,12 +3,18 @@ using OpenTK.Graphics.OpenGL;
 using OpenTK.Input;
 class Camera
 {
-    Vector3 CameraPosition = new Vector3(0, 0, 0), CameraDirection = new Vector3(0, 0, 0);
+    Vector3 CameraPosition = new Vector3(0, 0, 0), CameraDirection = new Vector3(0, 0, 0), ScreenCentre;
     float FOV;
+    Plane LeftScreen;
 
     public Camera()
     {
-
+        LeftScreen = new Plane();
+        LeftScreen.DistanceToOrigin = 20;
+        ScreenCentre = CameraPosition + CameraDirection * LeftScreen.DistanceToOrigin;
+        LeftScreen.P0 = ScreenCentre + new Vector3(-1, -1, 0);
+        LeftScreen.P1 = ScreenCentre + new Vector3(1, -1, 0);
+        LeftScreen.P2 = ScreenCentre + new Vector3(-1, 1, 0);
     }
 
     public void Tick()
--- Code/Raytracer.cs
+++ Code/Raytracer.cs
@@ -3,10 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using template.Code;
+using OpenTK.Graphics;
 
-namespace template.Code
+class Raytracer
 {
-    class Raytracer
+    Camera C;
+
+
+    public Raytracer()
     {
+        C = new Camera();
     }
+
+    void Render()
+    {
+
+    }
+
+struct Ray
+{
+    float3 O; // ray origin
+    float3 D; // ray direction
+    float t; // distance
+};
 }
