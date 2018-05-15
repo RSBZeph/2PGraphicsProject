38701287534aa19f88e8d337bc1a4ef575910837
Robert van den Brink <robertvdenbrink@gmail.com>
2018-05-15 11:13:47 +0200

    Larger Window

--- Code/Sphere.cs
+++ Code/Sphere.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OpenTK;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,5 +8,6 @@ using template.Code;
 
 class Sphere : Primitive
 {
-
+    Vector3 Position;
+    float Radius;
 }
--- Code/template.cs
+++ Code/template.cs
@@ -21,7 +21,7 @@ namespace Template
 			GL.Enable( EnableCap.Texture2D );
 			GL.Disable( EnableCap.DepthTest );
 			GL.Hint( HintTarget.PerspectiveCorrectionHint, HintMode.Nicest );
-			ClientSize = new Size( 640, 400 );
+			ClientSize = new Size( 1366, 720 );
 			game = new Game();
 			game.screen = new Surface( Width, Height );
 			Sprite.target = game.screen;
