9c6945fcfb1ca18a90ca5aaa4c74ac303e3d5f9c
Jeroen Hijzelendoorn <j.m.hijzelendoorn@students.uu.nl>
2018-05-15 13:21:53 +0200

    Raytracer & camera aanpassingen

--- Code/Raytracer.cs
+++ Code/Raytracer.cs
@@ -5,11 +5,12 @@ using System.Text;
 using System.Threading.Tasks;
 using template.Code;
 using OpenTK.Graphics;
+using OpenTK;
 
 class Raytracer
 {
     Camera C;
-
+    Vector3 O, D;
 
     public Raytracer()
     {
@@ -21,10 +22,12 @@ class Raytracer
 
     }
 
-struct Ray
-{
-    float3 O; // ray origin
-    float3 D; // ray direction
-    float t; // distance
-};
+    Vector3 CreateRayDirection(float x, float y)
+    {
+        Vector3 Direction, NDirection;
+        Vector3 ScreenPoint = C.LeftScreen.P0 + x * (C.LeftScreen.P1 - C.LeftScreen.P0) + y * (C.LeftScreen.P2 - C.LeftScreen.P0);
+        Direction = ScreenPoint - C.Position;
+        NDirection = Vector3.Divide(Direction, (float)Math.Sqrt(Math.Pow(Direction.X, 2) + Math.Pow(Direction.Y, 2) + Math.Pow(Direction.Z, 2)));
+        return Vector3.Divide(Direction, NDirection);
+    }
 }
