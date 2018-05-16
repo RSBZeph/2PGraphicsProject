cc5abe6673a6300aa5954aedaf07b8de908c123e
Jeroen Hijzelendoorn <j.m.hijzelendoorn@students.uu.nl>
2018-05-16 20:38:03 +0200

    .

--- Code/Scene.cs
+++ Code/Scene.cs
@@ -1,12 +1,33 @@
-﻿using System;
+﻿using OpenTK;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using template.Code;
 
-namespace template.Code
+class Scene
 {
-    class Scene
+    List<Sphere> spheres = new List<Sphere>();
+    List<Plane> planes = new List<Plane>();
+    List<Light> light = new List<Light>();
+    Raytracer r = new Raytracer();
+
+    public Scene()
     {
+
+    }
+
+    void FillLists()
+    {
+        Sphere s = new Sphere();
+        s.Position = new Vector3(3, 4, 7);
+        s.Radius = 3f;
+        spheres.Add(s);
+    }
+
+    void Intersect(Vector3 start, Vector3 direction)
+    {
+
     }
 }
