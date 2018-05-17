045f0b6800a2f8acb19443179de84ff023f24762
Robert van den Brink <robertvdenbrink@gmail.com>
2018-05-17 15:10:56 +0200

    ch

--- Code/Sphere.cs
+++ Code/Sphere.cs
@@ -12,4 +12,31 @@ class Sphere : Primitive
 {
     Vector3 Position;
     float Radius;
+
+    public Sphere()
+    {
+
+    }
+    //public Surface screen;
+    //int w, h;
+    public Sphere()
+    {
+        
+        //screen = new Surface(1366, 720);
+    }
+
+    public void Draw()
+    {
+     
+        
+        //for (double i = 0.0; i < 360; i++)
+        //{
+        //    double angle = i * Math.PI / 180;
+        //    int x = (int)(80 + 50 * Math.Cos(angle));
+        //    int y = (int)(80 + 50 * Math.Sin(angle));
+        //    int Location = x + y * screen.width;
+        //    screen.pixels[Location] = 255;
+        //}
+
+    }
 }
