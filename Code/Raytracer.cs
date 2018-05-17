5c024708b58fdb52519eb02e59fdf9a56b49e1fa
Jeroen Hijzelendoorn <j.m.hijzelendoorn@students.uu.nl>
2018-05-17 22:31:42 +0200

    aakjdf;asdjk

--- Code/Raytracer.cs
+++ Code/Raytracer.cs
@@ -8,8 +8,6 @@ class Raytracer
     Camera C;
     public Surface Screen;
     Scene S;
-    Vector3 DebugOrigin;
-    int scale = 50;
 
     public Raytracer(Surface sur)
     {
@@ -56,11 +54,10 @@ class Raytracer
     Vector3 CreateRayDirection(float x, float y)
     {
         //= unit vector
-        Vector3 Direction, NDirection;
+        Vector3 Direction;
         Vector3 ScreenPoint = C.LeftScreen.P0 + x * (C.LeftScreen.P1 - C.LeftScreen.P0) + y * (C.LeftScreen.P2 - C.LeftScreen.P0);
         Direction = ScreenPoint - C.Position;
-        NDirection = Vector3.Divide(Direction, (float)Math.Sqrt(Math.Pow(Direction.X, 2) + Math.Pow(Direction.Y, 2) + Math.Pow(Direction.Z, 2)));
-        return Vector3.Divide(Direction, NDirection);
+        return Vector3.Normalize(Direction);
     }
 
     public struct Ray
