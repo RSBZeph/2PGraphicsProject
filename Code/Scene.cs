5dc5d91661471d2b93253f9fd00ecfca0da6cdf5
Jeroen Hijzelendoorn <j.m.hijzelendoorn@students.uu.nl>
2018-05-17 11:26:52 +0200

    snijpunt met cirkel uitrekenen

--- Code/Scene.cs
+++ Code/Scene.cs
@@ -31,22 +31,38 @@ class Scene
     {
         foreach (Sphere sphere in spheres)
         {
-            for (float t = 0; t < 100; t += 0.5f)
-                if (Distance((ray.Start + ray.Direction * t), sphere.Position) <= sphere.Radius + 1)
-                {
-                    //if(sphere - intersect som die faking lang is > 0)
-                    //maakt nieuwe intersect instantie en vul met data
-                    //add aan intersections list
-                }
+            Vector3 L = sphere.Position - ray.Start;
+            float Lfloat = (float)Math.Sqrt(Math.Pow(L.X, 2) + Math.Pow(L.Y, 2) + Math.Pow(L.Z, 2)); //lengte van LenthToCentre (ABC formule)
+            float tc = Vector3.Dot(L, ray.Direction); //ray vanuit sphere.position evenwijdig aan ray en stopt bij ray.start
+            if (tc < 0)
+                continue;
+            float CentreToRay = (float)Math.Sqrt(Math.Pow(Lfloat, 2) + Math.Pow(tc, 2)); //loodlijn tussen ray en sphere.position (ABC formule)
+            if (CentreToRay > sphere.Radius)
+                continue;
+            float t1c = (float)Math.Sqrt(Math.Pow(sphere.Radius, 2) - Math.Pow(CentreToRay, 2)); //afstand snijpunt loodlijn - ray en snijpunt met cirkel (ABC formule)
+            float t1 = tc - t1c, t2 = tc + t1c; // lengte van ray.start naar eerste en tweede snijpunt met de sphere
+            Vector3 Point1 = ray.Start + ray.Direction * t1, Point2 = ray.Start + ray.Direction * t2; //eerste en tweede intersection point met de sphere            
+            Intersection i = new Intersection(), j = new Intersection();
+            i.Object = sphere;
+            i.Distance = t1;
+            i.Position = Point1;
+            intersections.Add(i);
+            j.Object = sphere;
+            j.Distance = t2;
+            i.Position = Point2;
+            intersections.Add(j);
+            break;
         }
 
         foreach(Plane plane in planes)
         {
             for (float t = 0; t < 100; t += 0.5f)
-                //if (ray raakt plane)
-                //{
-                //ditto van sphere
-                //}
+            {
+             //if (ray raakt plane)
+             //{
+             //ditto van sphere
+             //}
+            }
         }
     }
 
