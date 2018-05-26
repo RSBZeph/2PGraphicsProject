1d6ef314ce7928d8626873becfe2420f71f67e2c
Jeroen Hijzelendoorn <j.m.hijzelendoorn@students.uu.nl>
2018-05-26 16:20:13 +0200

    wowsers t werkt

--- Code/Scene.cs
+++ Code/Scene.cs
@@ -15,8 +15,9 @@ class Scene
     Intersection i1;
     public Surface Screen;
     float a, b, c, discriminant, result1, result2, finalresult, shadowlength, precalc1;
-    Vector3 difference, shadowray;
+    Vector3 difference1, difference2, difference3, shadowray;
     Ray SR;
+    float dot = 100;
 
     public Scene(Surface sur)
     {
@@ -26,17 +27,17 @@ class Scene
 
     void FillLists()
     {
-        Sphere s1 = new Sphere(new Vector3(4, 5, 7), 2f, new Vector3(0, 0.6f, 0.9f));
+        Sphere s1 = new Sphere(new Vector3(4, 5, 5), 1f, new Vector3(0, 0.6f, 0.9f));
         spheres.Add(s1);
 
-        Sphere s2 = new Sphere(new Vector3(6, 5, 4), 1f, new Vector3(0, 0.8f, 0.3f));
+        Sphere s2 = new Sphere(new Vector3(6, 5, 4), 0.2f, new Vector3(0, 0.8f, 0.3f));
         spheres.Add(s2);
 
-        Light l1 = new Light(new Vector3(0, 5, 2), 3f);
-        lights.Add(l1);
+        //Light l1 = new Light(new Vector3(0, 5, 2), 5f);
+        //lights.Add(l1);
 
-        //Light l2 = new Light(new Vector3(10, 5, 2), 4f);
-        //lights.Add(l2);
+        Light l2 = new Light(new Vector3(10, 5, 3), 7f);
+        lights.Add(l2);
     }
 
     public void DrawPrimitivesDebug()
@@ -74,10 +75,10 @@ class Scene
         finalresult = -1;
         foreach (Sphere sphere in spheres)
         {
-            difference = ray.Start - sphere.Position;
+            difference1 = ray.Start - sphere.Position;
             a = Vector3.Dot(ray.Direction, ray.Direction);
-            b = 2 * Vector3.Dot(difference, ray.Direction);
-            c = Vector3.Dot(difference, difference) - (sphere.Radius * sphere.Radius);
+            b = 2 * Vector3.Dot(difference1, ray.Direction);
+            c = Vector3.Dot(difference1, difference1) - (sphere.Radius * sphere.Radius);
             discriminant = (b * b) - (4 * a * c);
             if (discriminant > 0)
             {
@@ -96,22 +97,22 @@ class Scene
 
     public int ShadowRay(Intersection inter)
     {
-        float attenuation = 0.05f;
+        float attenuation = 0;
         foreach (Light light in lights)
         {
-            difference = light.Position - inter.Position;
-            shadowray = Vector3.Normalize(difference);
-            shadowlength = Math.Abs(Length(difference));
-            SR = new Ray(inter.Position - difference * 0.0001f, shadowray)
+            difference2 = light.Position - inter.Position;
+            shadowray = Vector3.Normalize(difference2);
+            shadowlength = Math.Abs(Length(difference2));
+            SR = new Ray(inter.Position - difference2 * 0.0001f, shadowray)
             {
                 x = inter.Ray.x,
                 y = inter.Ray.y,
                 MaxDistance = shadowlength,
             };
-            ShadowRayIntersect(inter, shadowlength);
+            ShadowRayIntersect();
             if (!SR.Occluded)
             {
-                attenuation += Vector3.Dot(inter.Normal, difference) / (shadowlength * shadowlength) * light.Intensity;
+                attenuation += Vector3.Dot(inter.Normal, difference2) / (shadowlength * shadowlength) * light.Intensity;
                 if (attenuation > 1)
                     attenuation = 1;
                 else if (attenuation < 0)
@@ -122,14 +123,14 @@ class Scene
         return Colour(inter.Color * attenuation);
     }
 
-    void ShadowRayIntersect(Intersection inter, float maxdis)
+    void ShadowRayIntersect()
     {
         foreach (Sphere sphere in spheres)
         {
-            difference = SR.Start - sphere.Position;
+            difference3 = SR.Start - sphere.Position;
             a = Vector3.Dot(SR.Direction, SR.Direction);
-            b = 2 * Vector3.Dot(difference, SR.Direction);
-            c = Vector3.Dot(difference, difference) - (sphere.Radius * sphere.Radius);
+            b = 2 * Vector3.Dot(difference3, SR.Direction);
+            c = Vector3.Dot(difference3, difference3) - (sphere.Radius * sphere.Radius);
             discriminant = (b * b) - (4 * a * c);
             if (discriminant > 0)
             {
@@ -138,7 +139,7 @@ class Scene
                 result2 = ((-b - precalc1) / (2 * a));
                 if(result1 > 0 && result2 > 0)
                 { 
-                    SR.Distance = Math.Min(result1, result1);
+                    SR.Distance = Math.Min(result1, result2);
                     SR.Occluded = true;
                     return;
                 }                
@@ -149,8 +150,8 @@ class Scene
 
     public float Distance(Vector3 first, Vector3 second)
     {
-        Vector3 Difference = second - first;
-        return (float)Math.Sqrt((Difference.X * Difference.X) + (Difference.Y * Difference.Y) + (Difference.Z * Difference.Z));
+        Vector3 offset = second - first;
+        return (float)Math.Sqrt((offset.X * offset.X) + (offset.Y * offset.Y) + (offset.Z * offset.Z));
     }
     public float Length(Vector3 L)
     {
