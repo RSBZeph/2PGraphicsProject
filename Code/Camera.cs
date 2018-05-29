b7664628ea510f190b6023c608ec8c89c439ada8
Jeroen Hijzelendoorn <j.m.hijzelendoorn@students.uu.nl>
2018-05-29 22:57:34 +0200

    D U N Z O

--- Code/Camera.cs
+++ Code/Camera.cs
@@ -4,7 +4,7 @@ using System;
 class Camera
 {
     public Vector3 Position = new Vector3(5, 5, 0.5f), Direction, ScreenCentre, P0, P1, P2, originaldirection = new Vector3(0, 0, 1);
-    public Vector3 Right = new Vector3(1, 0, 0), Up = new Vector3(0, 1, 0);
+    public Vector3 Right, Up = new Vector3(0, 1, 0);
     public float FOV = 180, ScreenWidth, DistanceToOrigin, DistanceToOrigin2D;
     static Camera C = new Camera();
 
@@ -13,14 +13,13 @@ class Camera
         //change the originaldirection to start out looking in a different direction
         Direction = Vector3.Normalize(originaldirection);
         ScreenWidth = 2;
-        Matrix4.CreateRotationX(4f);
+        Right = -Vector3.Cross(Direction, Up);
     }
 
     public void Tick()
     {
         DistanceToOrigin = (float)(1 / Math.Sin(((FOV / 2) / 180) * Math.PI));
         ScreenCentre = Position + Direction * DistanceToOrigin;
-        Console.WriteLine(ScreenCentre - Position);
         DistanceToOrigin2D = (float)Math.Sqrt((ScreenCentre - Position).X * (ScreenCentre - Position).X + (ScreenCentre - Position).Z * (ScreenCentre - Position).Z);
         P0 = ScreenCentre + Right + Up;
         P1 = ScreenCentre - Right + Up;
